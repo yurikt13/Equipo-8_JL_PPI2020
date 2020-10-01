fa4da178d246da8ebecfc74f10005fb3c7c383fc
perlayruvi <andresfelipeorozcocano@gmail.com>
2020-10-01 18:55:12 -0500

    agregue un hola mundo en el footer

--- frontend/src/pages/App1.js
+++ frontend/src/pages/App1.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Main1 from '../components/pantallas/Main1';
+import Footer from '../components/Footer';
 
 function App1() {
   return (
     <div className='App1'>
       <Header />
       <Main1 />
+      <Footer/>
 
     </div>
     );
